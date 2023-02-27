c39ba1c1b437d8b046799ce1aabf987d39de0cd6
Matheus Zanivan <zanivanandrade@gmail.com>
2023-02-27 19:22:52 -0300

    Projeto Criado

---
+++ handsOnCombine/handsOnCombine/handsOnCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  handsOnCombineApp.swift
+//  handsOnCombine
+//
+//  Created by Matheus Zanivan on 27/02/23.
+//
+
+import SwiftUI
+
+@main
+struct handsOnCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
